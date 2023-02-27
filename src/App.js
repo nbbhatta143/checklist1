ebde577a668a7418b090a29fdc349b996bb9e01f
nbbhatta143 <61528359+nbbhatta143@users.noreply.github.com>
2023-02-27 02:29:30 -0500

    router changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 import Checklist from "./component/Checklist";
-import ProfilePage from "./component/Profile";
 import Navigation from "./component/NavigationMenu";
+import ProfilePage from "./component/Profile";
 
-function App() {
-  return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route exact path="/" element={<Checklist />} />
-        <Route path="/profilePage" element={<ProfilePage />} />
-      </Routes>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <Navigation />
+        <Routes>
+          <Route exact path="/checklist" component={Checklist} />
+          <Route path="/profilePage" component={ProfilePage} />
+        </Routes>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
