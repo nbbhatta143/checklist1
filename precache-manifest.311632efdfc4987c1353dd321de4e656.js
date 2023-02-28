625199adf14b2c47c26a84f9d90b67191df4a61a
nbbhatta143 <61528359+nbbhatta143@users.noreply.github.com>
2023-02-28 01:53:04 -0500

    Updates

---
+++ precache-manifest.311632efdfc4987c1353dd321de4e656.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "7584573c1cc246312dac",
+    "url": "/checklist/static/css/main.adfb378e.chunk.css"
+  },
+  {
+    "revision": "7584573c1cc246312dac",
+    "url": "/checklist/static/js/main.7584573c.chunk.js"
+  },
+  {
+    "revision": "7bd77854c96d1f8250f1",
+    "url": "/checklist/static/js/1.7bd77854.chunk.js"
+  },
+  {
+    "revision": "7d1b5df4fa773911cd7b",
+    "url": "/checklist/static/css/2.33825d5a.chunk.css"
+  },
+  {
+    "revision": "7d1b5df4fa773911cd7b",
+    "url": "/checklist/static/js/2.7d1b5df4.chunk.js"
+  },
+  {
+    "revision": "64f6340a5ac6a2adab25",
+    "url": "/checklist/static/js/runtime~main.64f6340a.js"
+  },
+  {
+    "revision": "9b6e8613b2d646cfb80c68211f61bff8",
+    "url": "/checklist/static/media/Narayan.9b6e8613.jpg"
+  },
+  {
+    "revision": "83a42241ada98e0f959353ca0c3ee027",
+    "url": "/checklist/static/media/fa-brands-400.83a42241.woff2"
+  },
+  {
+    "revision": "47fa5e12e9fc4e8325a6e1a0735b38f6",
+    "url": "/checklist/static/media/fa-regular-400.47fa5e12.ttf"
+  },
+  {
+    "revision": "3f505ed834ca9466cf7efc2806b86604",
+    "url": "/checklist/static/media/fa-regular-400.3f505ed8.woff2"
+  },
+  {
+    "revision": "94227daa6303fd42918ce3af431ef7e8",
+    "url": "/checklist/static/media/fa-brands-400.94227daa.ttf"
+  },
+  {
+    "revision": "44f8148f80e6c3d60fa6425b0cbb862a",
+    "url": "/checklist/static/media/fa-solid-900.44f8148f.woff2"
+  },
+  {
+    "revision": "e8132d053030afa547e23b5163ef8301",
+    "url": "/checklist/static/media/fa-solid-900.e8132d05.ttf"
+  },
+  {
+    "revision": "91fc9b0078b939bddcde338550684848",
+    "url": "/checklist/static/media/fa-v4compatibility.91fc9b00.ttf"
+  },
+  {
+    "revision": "60cd2d66cb03cd2521862a12f970dbe9",
+    "url": "/checklist/static/media/fa-v4compatibility.60cd2d66.woff2"
+  },
+  {
+    "revision": "8da3a741e9cfb9577b23647b123f4671",
+    "url": "/checklist/index.html"
+  }
+];
